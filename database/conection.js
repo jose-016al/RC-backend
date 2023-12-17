469d19ccfb81a9b25e410d77475563cf6da16bcb
jose-016al <jose_016al@outlook.com>
2023-12-17 16:57:07 +0100

    Add backend

--- database/conection.js
+++ database/conection.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const conection = async() => {
 
     try {
-        await mongoose.connect("mongodb://localhost:27017/mi_redsocial");
+        /* await mongoose.connect("mongodb://localhost:27017/mi_redsocial"); */
+        await mongoose.connect("mongodb+srv://jose_016al:<password>@cluster1.secyv2u.mongodb.net/?retryWrites=true&w=majority");
         /* Parametros dentro de objeto, solo en caso de aviso */
         /* useNewUrlParser: true */
         /* useUnifiedTopology: true */
